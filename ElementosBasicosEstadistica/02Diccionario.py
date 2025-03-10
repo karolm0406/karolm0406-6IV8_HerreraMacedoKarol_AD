418da7565aa3197619e7b31320d6356238528890
karolm0406 <khrreram2200@alumno.ipn.mx>
2025-03-10 15:45:03 -0600

    apobados

--- ElementosBasicosEstadistica/02Diccionario.py
+++ ElementosBasicosEstadistica/02Diccionario.py
@@ -5,5 +5,9 @@ def estadistica_notas(notas):
     estadisticas =pd.Series([notas.min(), notas.max(), notas.mean(), notas.std()], index=['Min', 'Max', 'Media', 'Desviacion Estandar'])
     return estadisticas
 
+def aprobados(notas):
+    notas=pd.Series(notas)
+    return notas[notas >= 6].sort_values(ascending=False)
+
 notas={'Juan': 9, 'Juanita': 7, 'Pedro': 6.6, 'Fabian': 8.5, 'Maximiliano': 7.5, 'Rosario': 9},
 print(estadistica_notas(notas))

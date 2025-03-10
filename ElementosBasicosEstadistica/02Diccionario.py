dcf8942e6273df74fddd43c767e4b1c4c9dec681
karolm0406 <khrreram2200@alumno.ipn.mx>
2025-03-10 15:39:05 -0600

    diccionario

---
+++ ElementosBasicosEstadistica/02Diccionario.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def estadistica_notas(notas):
+    notas=pd.Series(notas)
+    estadisticas =pd.Series([notas.min(), notas.max(), notas.mean(), notas.std()], index=['Min', 'Max', 'Media', 'Desviacion Estandar'])
+    return estadisticas
+
+notas={'Juan': 9, 'Juanita': 7, 'Pedro': 6.6, 'Fabian': 8.5, 'Maximiliano': 7.5, 'Rosario': 9},
+print(estadistica_notas(notas))

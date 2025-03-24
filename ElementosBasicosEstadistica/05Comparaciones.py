531a4e27a2f5953f0313f0c0b49c2762f54cd16b
karolm006 <kherreram2200@alumno.ipn.mx>
2025-03-24 15:43:19 -0600

    avance2 05comparaciones

--- ElementosBasicosEstadistica/05Comparaciones.py
+++ ElementosBasicosEstadistica/05Comparaciones.py
@@ -18,9 +18,9 @@ sigma3 = 3
 n_muestras = 1000
 
 #vamos a generar los datos de las distribuciones normales
-data1 = np.random(media, sigma1, n_muestras)
-data2 = np.random(media, sigma2, n_muestras)
-data3 = np.random(media, sigma3, n_muestras)
+data1 =np.random.normal(media, sigma1, n_muestras)
+data2 = np.random.normal(media, sigma2, n_muestras)
+data3 = np.random.normal(media, sigma3, n_muestras)
 
 #Vamos a configurar la gráfica 
 plt.figure(figsize=(10,6))
@@ -28,9 +28,9 @@ plt.figure(figsize=(10,6))
 #vamos a cargar las frecuencias a partir de una gráfica de histogramas
 plt.hist(data1, bins=30, color='blue', density=True, label='Desviación Estandar 1', alpha=0.5)
 
-plt.hist(data2, bins=30, color='blue', density=True, label='Desviación Estandar 2', alpha=0.5)
+plt.hist(data2, bins=30, color='red', density=True, label='Desviación Estandar 2', alpha=0.5)
 
-plt.hist(data3, bins=30, color='blue', density=True, label='Desviación Estandar 3', alpha=0.5)
+plt.hist(data3, bins=30, color='green', density=True, label='Desviación Estandar 3', alpha=0.5)
 
 #a graficar
 plt.title('Comparación de Distribuciones Normales con una semilla en random')
